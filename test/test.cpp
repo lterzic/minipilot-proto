6ff2f5dcc79f9e5e13f5287c2a6a71672d8cc995
Luka Terzic <lukaterzicbg@gmail.com>
2025-05-16 10:42:27 +0200

    package hierarchy now starts from pb

--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    mpsim::pb::ResponseGetSimData get_sim_data;
-    const mp::pb::TelemetryMessage& msg = get_sim_data.sim_data();
+    pb::mpsim::Response response;
+    const pb::mp::TelemetryMessage& msg = response.get_sim_data().sim_telemetry();
     std::cout << msg.has_sensor_data() << std::endl;
     return 0;
 }
